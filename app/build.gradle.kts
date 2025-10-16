8e8236c2a0d1911452ab5b5c65911927c38f50d1
fikrifaizal <fikrifaizal40@gmail.com>
2025-10-16 17:19:50 +0700

    build[Libs]: menambahkan beberapa library yang akan digunakan

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("kotlin-kapt")
+    id("com.google.devtools.ksp")
+    alias(libs.plugins.hiltAndroid)
 }
 
 android {
@@ -33,15 +36,59 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
+    }
 }
 
 dependencies {
-
+//    Kotlin
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
+
+//    UI
     implementation(libs.material)
+    implementation(libs.androidx.appcompat)
     implementation(libs.androidx.activity)
+    implementation(libs.androidx.fragment)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.recyclerview)
+    implementation(libs.androidx.swiperefreshlayout)
+
+//    Navigation
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+
+//    Architecture KTX
+    implementation(libs.androidx.activity.ktx)
+    implementation(libs.androidx.fragment.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+
+//    Networking
+    implementation (libs.retrofit2)
+    implementation (libs.retrofit2.converter.gson)
+    implementation (libs.logging.interceptor)
+
+//    Database and Paging
+    implementation (libs.androidx.room.runtime)
+    implementation (libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
+    implementation (libs.androidx.room.paging)
+    implementation (libs.androidx.paging.runtime.ktx)
+
+//    Datastore
+    implementation(libs.androidx.datastore.preferences)
+
+//    Coroutine
+    implementation (libs.kotlinx.coroutines.android)
+
+//    Dependency Injection
+//    Dagger Hilt cannot use ksp instead kapt
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.android.compiler)
+
+//    Testing
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
